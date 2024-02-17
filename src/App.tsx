1c4d91d56b22e0c2900996cf50a7a2569e33a67f
emiraelDev <emmzex19@gmail.com>
2024-02-17 01:22:23 +0100

    add: cover images, more packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Welcome } from "./pages/welcome/Welcome";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-gray-900 h-[100vh] text-gray-100 font-roboto">
+      <Welcome />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/welcome/Welcome.tsx
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <div className=" w-full m-auto text-center ">
+      <div className=" font-poppins">emirael todo</div>
+    </div>
+  );
+}
